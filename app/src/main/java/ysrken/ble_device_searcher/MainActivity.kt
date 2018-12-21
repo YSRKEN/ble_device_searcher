cae55966b1383beaaeb360c7ed48fe7170ccf439
YSRKEN <ysr.ken@gmail.com>
2018-12-21 21:38:56 +0900

    initial project

---
+++ app/src/main/java/ysrken/ble_device_searcher/MainActivity.kt
@@ -0,0 +1,12 @@
+package ysrken.ble_device_searcher
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
